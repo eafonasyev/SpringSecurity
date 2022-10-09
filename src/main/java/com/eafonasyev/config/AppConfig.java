dae90fb898b351501749d4e8fae4a997763d12e9
Evgeniy <eafonasyev@gmail.com>
2022-10-09 18:09:29 +0600

    FirstCommit

---
+++ src/main/java/com/eafonasyev/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.eafonasyev.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.eafonasyev")
+public class AppConfig {
+
+//ENABLE A Bean ViewResolver
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
